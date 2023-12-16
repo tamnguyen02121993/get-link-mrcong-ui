4b66e3bd7b1101ca701691b65d56ecb9030c3074
tamnguyen02121993 <tamnguyen02121993@gmail.com>
2023-12-16 16:23:32 +0700

    Add global object for react lightbox

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,6 @@ export default defineConfig(({ command, mode }) => {
   process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
   return {
     plugins: [react()],
-    define: process.env.NODE_ENV === "development" ? { global: "window" } : {},
+    define: { global: "window" },
   };
 });
