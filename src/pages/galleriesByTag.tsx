450f2e785c662f79dfacfe70dd6385e0974279a6
tamnguyen02121993 <tamnguyen02121993@gmail.com>
2023-12-20 12:45:53 +0700

    Add trending for home and tag page

--- src/pages/galleriesByTag.tsx
+++ src/pages/galleriesByTag.tsx
@@ -1,11 +1,12 @@
 import { Navigate, useParams } from "react-router-dom";
-import { GalleryItem, Loading, Pagination } from "../components";
+import { GalleryItem, Loading, Pagination, TrendingItem } from "../components";
 import { useQuery } from "@tanstack/react-query";
-import { Gallery, GalleriesWithTrending } from "../interfaces";
+import { Gallery, GalleriesWithTrending, Trending } from "../interfaces";
 import { QUERY_KEYS } from "../commons/keys";
 import { getGalleriesByTag } from "../apis";
 import { usePaginationStore } from "../store";
 import { useInitData } from "../hooks";
+import { PiStarFourBold } from "react-icons/pi";
 
 const GalleriesByTag: React.FC = () => {
   useInitData();
@@ -22,18 +23,34 @@ const GalleriesByTag: React.FC = () => {
     staleTime: import.meta.env.VITE_APP_STALE_TIME,
   });
   return (
-    <>
-      <section className="flex flex-col gap-y-4">
-        {isLoadingError && Navigate({ to: "/error" })}
-        {isLoading && !isLoadingError && <Loading />}
+    <div className="flex flex-row flex-wrap md:flex-nowrap gap-4">
+      {isLoadingError && Navigate({ to: "/error" })}
+      {isLoading && !isLoadingError && <Loading />}
+      <section className="flex flex-col gap-y-4 md:flex-grow">
         {!isLoading &&
           !isLoadingError &&
           data?.items.map((gallery: Gallery) => (
             <GalleryItem gallery={gallery} key={gallery.title} />
           ))}
       </section>
+      <section className="border-gray-200 pl-4 md:border-l lg:basis-[350px] md:basis-[250px] md:flex-shrink-0">
+        {!isLoading && !isLoadingError && (
+          <h3 className="font-semibold text-xl text-gray-700 mb-4 flex flex-row items-center gap-x-2">
+            <PiStarFourBold />
+            <span>Trending</span>
+            <PiStarFourBold />
+          </h3>
+        )}
+        <div className="flex flex-col gap-y-4">
+          {!isLoading &&
+            !isLoadingError &&
+            data?.trending.map((trending: Trending) => (
+              <TrendingItem {...trending} key={trending.href} />
+            ))}
+        </div>
+      </section>
       <Pagination />
-    </>
+    </div>
   );
 };
 
