d0905031b33deb14003206c8349c47b67d2a2b94
tamnguyen02121993 <tamnguyen02121993@gmail.com>
2024-01-25 01:19:29 +0700

    Update UI change endpoint

--- src/components/endpoint.tsx
+++ src/components/endpoint.tsx
@@ -1,6 +1,5 @@
 import { createPortal } from "react-dom";
 import { IoMdSwap } from "react-icons/io";
-import { SiVercel, SiRender } from "react-icons/si";
 import { useApiEndpointStore } from "../store";
 import { DEFAULT_ENDPOINTS } from "../commons";
 
@@ -16,20 +15,14 @@ function Endpoint() {
     }
   };
   return createPortal(
-    <div className="shadow-md fixed bottom-[60px] lg:bottom-[80px] right-5">
-      <div
-        className="flex flex-col items-center px-4 py-2 bg-pink-300 hover:bg-pink-400 text-white rounded-lg cursor-pointer"
-        onClick={handleChangeEndpoint}
-      >
-        {selectedEndpoint.name === "Vercel" ? (
-          <SiVercel size={30} />
-        ) : (
-          <SiRender size={30} />
-        )}
-        <span className="font-light text-sm select-none">
-          {selectedEndpoint.name}
-        </span>
-        <IoMdSwap size={24} />
+    <div className="fixed top-0 right-0 w-[140px] h-6 px-2 bg-pink-300 text-white">
+      <div className="flex items-center justify-between">
+        <span className="text-sm">Server: {selectedEndpoint.name}</span>
+        <IoMdSwap
+          className="cursor-pointer hover:text-pink-400"
+          size={20}
+          onClick={handleChangeEndpoint}
+        />
       </div>
     </div>,
     document.querySelector("body")!
